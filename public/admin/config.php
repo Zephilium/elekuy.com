7fe58d293e65f3a263de3145e036c5d5739a10a5
Zephilium <wuuuuush@gmail.com>
2021-07-15 14:20:04 +0700

    first commit

---
+++ public/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://elekuy.com/admin/');
+define('HTTP_CATALOG', 'http://elekuy.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://elekuy.com/admin/');
+define('HTTPS_CATALOG', 'http://elekuy.com/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/elekuy.com/public/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/elekuy.com/public/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/elekuy.com/public/image/');
+define('DIR_STORAGE', 'C:/xampp/htdocs/elekuy.com/public_html/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/elekuy.com/public/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
